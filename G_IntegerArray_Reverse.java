294961dbed5a93121d9c9337d155d0702a33f820
Prathmesh.Gorle <prathmeshgorle4@gmail.com>
2023-09-28 15:27:41 +0530

    G_IntegerArray_Reverse

---
+++ G_IntegerArray_Reverse.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class G_IntegerArray_Reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of Array: ");
+        int size = sc.nextInt();
+        int [] numbers = new int[size];
+       
+        //input
+        for (int i=0;i<size;i++){
+            numbers[i] =sc.nextInt();
+        }
+      
+        for(int i=0;i<size;i++){
+            System.out.println(numbers[i]);
+        }
+
+    //Reverse Array
+    for(int i=size-1;i>=0;i--){
+        System.out.print(numbers[i]+" ");
+    }
+
+    }
+}
